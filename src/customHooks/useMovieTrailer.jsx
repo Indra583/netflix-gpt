0941723befdc52dcaf80a178f8511b60ddf48679
Indrashekar <indrashekar19@gmail.com>
2024-03-27 13:38:04 +0530

    memiozation

--- src/customHooks/useMovieTrailer.jsx
+++ src/customHooks/useMovieTrailer.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addMoviesTrailer } from "../utils/moviesSlice";
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
+  const moviesTrailer = useSelector((store) => store.movies.moviesTrailer)
 
   const getMovieVideos = async () => {
     const data = await fetch(
@@ -24,7 +25,7 @@ const useMovieTrailer = (movieId) => {
   };
 
   useEffect(() => {
-    getMovieVideos();
+    if(!moviesTrailer) getMovieVideos();
   }, []);
 };
 
